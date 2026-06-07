2ede7c4dd88f1ef4df545ab3a3c3da3b62f1e1fd
PhysicistSouravDas <souravdas.sd20@gmail.com>
2026-06-07 10:56:48 +0000

    Update secret key, admin url, and api endpoint in activation page

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ghkn31%5vsysrx2g3n@a*n)-gi$d7-5i464kcoej9^=d2r5*=c'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+admin_url = os.environ.get('ADMIN_URL', 'admin/')
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(admin_url, admin.site.urls),
     path('api/focus/', include('focus.urls')),
     # login endpoint to get the tokens
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
